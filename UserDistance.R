770fbf6599acb3a9b14b016ed718ec46a4c7319f
abhimanyuhazarika <32271162+abhimanyuhazarika@users.noreply.github.com>
2017-12-18 03:16:27 +0000

    Update UserDistance.R
    Added working directory and Export result to CSV feature

--- UserDistance.R
+++ UserDistance.R
@@ -1,8 +1,11 @@
 install.packages("jsonlite")
 library("jsonlite")
 
+#set the working directory
+setwd("U:/IntercomTest") 
+
 # Retrieving the user data stored in JSON format
-data<-stream_in(file("U:/gistfile1.txt"))
+data<-stream_in(file("gistfile1.txt"))
 
 # Function to convert degree values to radian
 deg2rad <- function(deg) return(deg*pi/180)
@@ -13,19 +16,22 @@ data$longitude <- deg2rad(as.numeric(data$longitude))
 
 # Function to calculate the geo distance between User's latitude/longitude and Dublin office 
 # using Spherical Law of Cosines
-distance <- function(long1, lat1) {
-  long2=deg2rad(-6.257664) #Longitude of Intercom's Dublin office
-  lat2<-deg2rad(53.339428) #Latitude of Intercom's Dublin office
+distance <- function(usrLong, usrLat) {
+  officeLong=deg2rad(-6.257664) #Longitude of Intercom's Dublin office
+  officeLat<-deg2rad(53.339428) #Latitude of Intercom's Dublin office
   R <- 6371 # Mean radius of Earth
-  dist <- acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2) * cos(long2-long1)) * R
+  dist <- acos(sin(usrLat)*sin(officeLat) + cos(usrLat)*cos(officeLat) * cos(officeLong-usrLong)) * R
   return(dist) # Returning distance in kms
 }
 
-# Adding column distance containing distance of the user from Intercom's Dublin office
+# Adding column 'distance' containing distance of the user from Intercom's Dublin office
 data$distance<-distance(data$longitude, data$latitude)
 
-# Selecting only the user_id and name of the customers who live within 100 kms from Intercom's Dublin office
+# Selecting only the 'user_id' and 'name' of the customers who live within 100 kms from Intercom's Dublin office
 data<-data[data$distance <= 100, c("user_id","name")] 
 
 # Putting the data in ascending order of user id
 data <-data[order(data$user_id),]
+
+#Export the data into a CSV file
+write.csv(data,file='usersWithin100kms.csv')
